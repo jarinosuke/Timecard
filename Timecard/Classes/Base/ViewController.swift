71fa613a039c100da79b973c286d86d2896ae201
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2014-12-23 14:54:09 +0900

    Created base VC.

---
+++ Timecard/Classes/Base/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Timecard
+//
+//  Created by Naoki Ishikawa on 12/23/14.
+//  Copyright (c) 2014 Naoki Ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
